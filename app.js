072586ff757c08421371ff9501883f26fd5cc07f
Zoher Rangwala <153892122+zoherr@users.noreply.github.com>
2024-06-21 20:03:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ io.on("connection", function (uniquesocket) {
     })
 })
 
-server.listen(3000, () => {
-    console.log("Server Started");
-})
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
